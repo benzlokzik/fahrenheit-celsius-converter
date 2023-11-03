dba62da3bb6f0def7c7709074e4bf0abce02b9ef
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-11-03 06:00:17 +0300

    added temperature conversion models

---
+++ src/models/mod.rs
@@ -0,0 +1,1 @@
+pub mod temperature_conversion;
---
+++ src/models/temperature_conversion.rs
@@ -0,0 +1,5 @@
+// Struct for temperature conversion data pairs
+pub struct TemperatureConversion {
+    pub celsius: f64,
+    pub fahrenheit: f64,
+}
