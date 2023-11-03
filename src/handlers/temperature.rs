1ae80d0ccae0aa2628dc29c8e4d65d98ab97151e
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-11-03 15:35:12 +0300

    corrected `temperature.rs`

--- src/handlers/temperature.rs
+++ src/handlers/temperature.rs
@@ -1,12 +1,14 @@
 use actix_web::{web, HttpResponse, Responder};
 use crate::models::temperature_conversion::{Celsius, Fahrenheit};
 
-pub async fn celsius_to_fahrenheit(web::Path(temp): web::Path<f64>) -> impl Responder {
+pub async fn celsius_to_fahrenheit(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
     let fahrenheit = Celsius::new(temp).to_fahrenheit();
     HttpResponse::Ok().json(fahrenheit)
 }
 
-pub async fn fahrenheit_to_celsius(web::Path(temp): web::Path<f64>) -> impl Responder {
+pub async fn fahrenheit_to_celsius(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
     let celsius = Fahrenheit::new(temp).to_celsius();
     HttpResponse::Ok().json(celsius)
 }
