fbff28b2b46b538e573445ba0e8d37bfa69b896c
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-11-06 03:53:05 +0300

    added Kelvin model and converter functions

--- src/models/temperature_conversion.rs
+++ src/models/temperature_conversion.rs
@@ -10,6 +10,11 @@ pub struct Fahrenheit {
     pub fahrenheit: f64,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Kelvin {
+    pub kelvin: f64,
+}
+
 impl Celsius {
     pub fn new(celsius: f64) -> Self {
         Celsius { celsius }
@@ -18,6 +23,12 @@ impl Celsius {
     pub fn to_fahrenheit(&self) -> Fahrenheit {
         Fahrenheit {
             fahrenheit: (self.celsius * 1.8) + 32.0,
+        }
+    }
+
+    pub fn to_kelvin(&self) -> Kelvin {
+        Kelvin {
+            kelvin: self.celsius + 273.15,
         }
     }
 }
@@ -30,6 +41,30 @@ impl Fahrenheit {
     pub fn to_celsius(&self) -> Celsius {
         Celsius {
             celsius: (self.fahrenheit - 32.0) / 1.8,
+        }
+    }
+
+    pub fn to_kelvin(&self) -> Kelvin {
+        Kelvin {
+            kelvin: (self.fahrenheit - 32.0) / 1.8 + 273.15,
+        }
+    }
+}
+
+impl Kelvin {
+    pub fn new(kelvin: f64) -> Self {
+        Kelvin { kelvin }
+    }
+
+    pub fn to_celsius(&self) -> Celsius {
+        Celsius {
+            celsius: self.kelvin - 273.15,
+        }
+    }
+
+    pub fn to_fahrenheit(&self) -> Fahrenheit {
+        Fahrenheit {
+            fahrenheit: (self.kelvin - 273.15) * 1.8 + 32.0,
         }
     }
 }
