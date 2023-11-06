a670c3d91a8c75122877dad0747bff0b0d02f1e5
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-11-06 04:07:00 +0300

    added http response handlers for Kelvin

--- src/app.rs
+++ src/app.rs
@@ -5,6 +5,10 @@ pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/convert")
             .route("/c_to_f/{temp}", web::get().to(temperature::celsius_to_fahrenheit))
-            .route("/f_to_c/{temp}", web::get().to(temperature::fahrenheit_to_celsius)),
+            .route("/f_to_c/{temp}", web::get().to(temperature::fahrenheit_to_celsius))
+            .route("/k_to_c/{temp}", web::get().to(temperature::kelvin_to_celsius))
+            .route("/c_to_k/{temp}", web::get().to(temperature::celsius_to_kelvin))
+            .route("/k_to_f/{temp}", web::get().to(temperature::kelvin_to_fahrenheit))
+            .route("/f_to_k/{temp}", web::get().to(temperature::fahrenheit_to_kelvin)),
     );
 }
--- src/handlers/temperature.rs
+++ src/handlers/temperature.rs
@@ -1,5 +1,5 @@
 use actix_web::{web, HttpResponse, Responder};
-use crate::models::temperature_conversion::{Celsius, Fahrenheit};
+use crate::models::temperature_conversion::{Celsius, Fahrenheit, Kelvin};
 
 pub async fn celsius_to_fahrenheit(temp: web::Path<f64>) -> impl Responder {
     let temp = temp.into_inner();
@@ -11,4 +11,28 @@ pub async fn fahrenheit_to_celsius(temp: web::Path<f64>) -> impl Responder {
     let temp = temp.into_inner();
     let celsius = Fahrenheit::new(temp).to_celsius();
     HttpResponse::Ok().json(celsius)
+}
+
+pub async fn kelvin_to_celsius(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
+    let celsius = Kelvin::new(temp).to_celsius();
+    HttpResponse::Ok().json(celsius)
+}
+
+pub async fn celsius_to_kelvin(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
+    let kelvin = Celsius::new(temp).to_kelvin();
+    HttpResponse::Ok().json(kelvin)
+}
+
+pub async fn kelvin_to_fahrenheit(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
+    let fahrenheit = Kelvin::new(temp).to_fahrenheit();
+    HttpResponse::Ok().json(fahrenheit)
+}
+
+pub async fn fahrenheit_to_kelvin(temp: web::Path<f64>) -> impl Responder {
+    let temp = temp.into_inner();
+    let kelvin = Fahrenheit::new(temp).to_kelvin();
+    HttpResponse::Ok().json(kelvin)
 }
